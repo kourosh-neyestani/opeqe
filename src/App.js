0ffd0d2bb131ad714f89681b574af910cf40575b
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-06-21 19:57:29 +0430

    add app router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div>
-            <h1>Welcome to Opeqe.</h1>
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route
+                        path="/"
+                        component={() => (
+                            <div>
+                                <h1>Home</h1>
+                            </div>
+                        )}
+                    />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
